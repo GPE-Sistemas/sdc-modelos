481cfc3e7f9efcfb0aa68746a1c9d7a6dfedfe58
Tatoh <tatukis@gmail.com>
2022-03-08 16:18:03 -0300

    Notas

--- src/entidades/usuarios/usuario.create.ts
+++ src/entidades/usuarios/usuario.create.ts
@@ -1,13 +1,14 @@
 import { UsuarioRol } from '../../compartidos/usuarioRoles';
 
 export interface ICreateUsuario {
+  // Datos enviados desde el frontend
   username: string;
   passowrd?: string;
   email: string;
   firstName: string;
   lastName: string;
   rol?: UsuarioRol;
-  //
+  // Datos rellenados por la API
   hash?: string;
   salt?: string;
   updatedAt?: string;
--- src/entidades/usuarios/usuario.update.ts
+++ src/entidades/usuarios/usuario.update.ts
@@ -1,13 +1,14 @@
 import { UsuarioRol } from '../../compartidos/usuarioRoles';
 
 export interface IUpdateUsuario {
+  // Datos enviados desde el frontend
   password?: string;
   email?: string;
   firstName?: string;
   lastName?: string;
   rol?: UsuarioRol;
   active?: boolean;
-  //
+  // Datos rellenados por la API
   hash?: string;
   salt?: string;
   updatedAt?: string;
