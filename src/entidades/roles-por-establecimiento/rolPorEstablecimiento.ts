a9b1bc47768abb88e501b98131fab2a2168b51b5
Tatoh <tatukis@gmail.com>
2022-03-21 17:41:12 -0300

    id

--- src/entidades/roles-por-establecimiento/rolPorEstablecimiento.ts
+++ src/entidades/roles-por-establecimiento/rolPorEstablecimiento.ts
@@ -1,6 +1,7 @@
-import { UsuarioRol } from "../../compartidos/usuarioRoles";
+import { UsuarioRol } from '../../compartidos/usuarioRoles';
 
 export interface IRolPorEstablecimiento {
+  _id: string;
   idUsuario: string;
   idEStablecimiento: string;
   rol: UsuarioRol;
