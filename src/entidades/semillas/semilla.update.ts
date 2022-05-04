753673beed007ca7c96d7d95d5c6e95796bd1462
Tatoh <tatukis@gmail.com>
2022-05-04 12:38:36 -0300

    Semilla

---
+++ src/entidades/semillas/semilla.update.ts
@@ -0,0 +1,10 @@
+export interface IUpdateSemilla {
+  // Estoy tirando fruta.
+  // ex: Soja, maní, etc
+  tipoDeCultivo?: string;
+  // Marca comercial
+  nombreComercial?: string;
+  // Nombre
+  nombre?: string;
+  // No sé que más
+}
