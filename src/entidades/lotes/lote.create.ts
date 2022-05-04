ae88be4d0733fd2767bbd3c2592a68b29f3bcf29
Tatoh <tatukis@gmail.com>
2022-05-04 16:39:41 -0300

    Jony, del uno al diez, eres un idiota.

--- src/entidades/lotes/lote.create.ts
+++ src/entidades/lotes/lote.create.ts
@@ -3,9 +3,4 @@ import { IUbicacion } from '../../compartidos/ubicacion';
 export interface ICreateLote {
   nombre: string;
   ubicacion: IUbicacion;
-  createdAt: string;
-  upDatedAt: string;
-  deletedAt?: string;
-  active: boolean;
-  idEstablecimiento: string;
 }
