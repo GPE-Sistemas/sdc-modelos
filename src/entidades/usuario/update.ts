35daf1ba630f1ff8159853165a1d032b5e2aad67
Fernando Cortese <cortesefernando@gmail.com>
2022-06-22 14:43:26 -0300

    upd user

--- src/entidades/usuario/update.ts
+++ src/entidades/usuario/update.ts
@@ -1,11 +1,10 @@
 import { IDatosPersonales, IPermiso } from "./schema";
 
 export interface IUpdateUsuario {
-  _id: string;
   activo?: boolean;
   username?: string;
   password?: string;
   hash?: string;
-  permisos: IPermiso[];
+  permisos?: IPermiso[];
   datosPersonales?: IDatosPersonales;
 }
