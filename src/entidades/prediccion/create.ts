a23ac6f35f1b2ef64f3fbdbcd3e406b89e42558c
Fernando Cortese <cortesefernando@gmail.com>
2022-07-01 12:17:50 -0300

    prediccion

--- src/entidades/prediccion/create.ts
+++ src/entidades/prediccion/create.ts
@@ -3,6 +3,6 @@ import { IPrediccionEnfermedad, IPrediccionEstacion } from "./schema";
 export interface ICreatePrediccion {
   fecha: string;
   idSiembra: string;
-  enfermedades: IPrediccionEnfermedad[];
-  estacion: IPrediccionEstacion;
+  enfermedades?: IPrediccionEnfermedad[];
+  estacion?: IPrediccionEstacion;
 }
