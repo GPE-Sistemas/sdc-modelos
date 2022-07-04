c89f19edfd3c06b99b3ae6c25584026238d1cea0
Fernando Cortese <cortesefernando@gmail.com>
2022-07-04 10:33:10 -0300

    update ubicacion

--- src/compartidos/ubicacion.ts
+++ src/compartidos/ubicacion.ts
@@ -1,7 +1,8 @@
-import { ICoordenadas } from './coordenadas';
+import { ICoordenadas } from "./coordenadas";
 
 export interface IUbicacion {
   poligono: ICoordenadas[];
+  centro?: ICoordenadas;
   nombre?: string;
   superficie?: number;
 }
