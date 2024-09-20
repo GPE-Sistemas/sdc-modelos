d510616d10f7fa0e202649466ed30b90bc9b8e3f
Fernando Cortese <cortesefernando@gmail.com>
2024-09-20 14:12:12 -0300

    refactor: Actualizar importaciones en index.ts y agregar apikey.ts

--- src/entidades/apikey.ts
+++ src/entidades/apikey.ts
@@ -1,7 +1,7 @@
-import { IDistribuidor } from './distribuidor';
-import { IProductor } from './productor';
-import { IQuimica } from './quimica';
-import { IPermiso } from './usuario';
+import { IDistribuidor } from "./distribuidor";
+import { IProductor } from "./productor";
+import { IQuimica } from "./quimica";
+import { IPermiso } from "./usuario";
 
 export interface IApikey {
   _id?: string;
@@ -9,21 +9,13 @@ export interface IApikey {
   fechaCreacion?: string;
   identificacion?: string;
   key?: string;
-  // Permisos
-  idProductor?: string;
-  idDistribuidor?: string;
-  idQuimica?: string;
   permisos?: IPermiso[];
-  // Populate
-  productor?: IProductor;
-  distribuidor?: IDistribuidor;
-  quimica?: IQuimica;
 }
 
-type OmitirCreate = '_id' | 'productor' | 'distribuidor' | 'quimica';
+type OmitirCreate = "_id" | "productor" | "distribuidor" | "quimica";
 
 export interface ICreateApikey extends Omit<Partial<IApikey>, OmitirCreate> {}
 
-type OmitirUpdate = '_id' | 'productor' | 'distribuidor' | 'quimica';
+type OmitirUpdate = "_id" | "productor" | "distribuidor" | "quimica";
 
 export interface IUpdateApikey extends Omit<Partial<IApikey>, OmitirUpdate> {}
