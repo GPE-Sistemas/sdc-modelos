b1a5906e23260392c81cbd557820b28ffbd6d1f2
Tatoh <tatukis@gmail.com>
2025-03-26 17:54:17 -0300

    Refactor: Mejorar la consistencia de las comillas en el archivo lote.ts y añadir la interfaz ICalidadClima

--- src/entidades/lote.ts
+++ src/entidades/lote.ts
@@ -1,32 +1,32 @@
-import { IUbicacion } from "../compartidos/ubicacion";
-import { IDepartamento } from "./departamento";
-import { IDistribuidor } from "./distribuidor";
-import { IEstablecimiento } from "./establecimiento";
-import { IEstacion } from "./estacion";
-import { IProductor } from "./productor";
-import { IQuimica } from "./quimica";
-import { IHuellaHidrica, ISiembra } from "./siembra";
+import { IUbicacion } from '../compartidos/ubicacion';
+import { IDepartamento } from './departamento';
+import { IDistribuidor } from './distribuidor';
+import { IEstablecimiento } from './establecimiento';
+import { IEstacion } from './estacion';
+import { IProductor } from './productor';
+import { IQuimica } from './quimica';
+import { IHuellaHidrica, ISiembra } from './siembra';
 
 export type TTexturaSuelo =
-  | "Arcilloso"
-  | "Franco arcilloso"
-  | "Franco"
-  | "Franco arenoso"
-  | "Arenoso";
+  | 'Arcilloso'
+  | 'Franco arcilloso'
+  | 'Franco'
+  | 'Franco arenoso'
+  | 'Arenoso';
 export type TTipoDrenaje =
-  | "Mal Drenado"
-  | "Moderadamente Drenado"
-  | "Bien Drenado"
-  | "Excesivamente Drenado";
+  | 'Mal Drenado'
+  | 'Moderadamente Drenado'
+  | 'Bien Drenado'
+  | 'Excesivamente Drenado';
 
 export type TTipoErosionEscorrentiaPendiente =
-  | "Baja (0 - 3%)"
-  | "Moderada (3 - 8%)"
-  | "Alta (8 - 15%)"
-  | "Muy Alta (> 15%)";
-export type TTipoContenidoP = "< 12" | "> 12 < 20" | "> 20 < 30" | "> 30";
+  | 'Baja (0 - 3%)'
+  | 'Moderada (3 - 8%)'
+  | 'Alta (8 - 15%)'
+  | 'Muy Alta (> 15%)';
+export type TTipoContenidoP = '< 12' | '> 12 < 20' | '> 20 < 30' | '> 30';
 
-export type TTipoDepositoN = "< 0.5" | "> 0.5" | "< 1.5" | "> 1.5";
+export type TTipoDepositoN = '< 0.5' | '> 0.5' | '< 1.5' | '> 1.5';
 
 export interface ISuelo {
   profundidad?: number;
@@ -35,6 +35,11 @@ export interface ISuelo {
   capacidadDeCampo?: number;
   puntoMarchitez?: number;
   numeroDeSensor?: number;
+}
+
+export interface ICalidadClima {
+  fecha?: string;
+  nivel?: number; // 1 - Excelente, 2 - Bueno, 3 - Malo
 }
 
 export interface ILote {
@@ -65,6 +70,7 @@ export interface ILote {
   huellaHidrica?: IHuellaHidrica;
   // Id de la ultima siembra del lote
   idSiembra?: string;
+  calidadClima?: ICalidadClima;
   // Populate
   quimica?: IQuimica;
   distribuidor?: IDistribuidor;
@@ -76,21 +82,21 @@ export interface ILote {
 }
 
 type OmitirCreate =
-  | "_id"
-  | "quimica"
-  | "distribuidor"
-  | "productor"
-  | "establecimiento"
-  | "departamento"
-  | "sondaSuelo";
+  | '_id'
+  | 'quimica'
+  | 'distribuidor'
+  | 'productor'
+  | 'establecimiento'
+  | 'departamento'
+  | 'sondaSuelo';
 export interface ICreateLote extends Omit<Partial<ILote>, OmitirCreate> {}
 
 type OmitirUpdate =
-  | "_id"
-  | "quimica"
-  | "distribuidor"
-  | "productor"
-  | "establecimiento"
-  | "departamento"
-  | "sondaSuelo";
+  | '_id'
+  | 'quimica'
+  | 'distribuidor'
+  | 'productor'
+  | 'establecimiento'
+  | 'departamento'
+  | 'sondaSuelo';
 export interface IUpdateLote extends Omit<Partial<ILote>, OmitirUpdate> {}
