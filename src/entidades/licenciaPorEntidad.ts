c559c13515ca3ebf2422e2f981f20184143bee97
Tatoh <tatukis@gmail.com>
2025-06-04 16:55:00 -0300

    Refactor: Update ILicenciaPorEntidad interface to use proper types for virtual properties

--- src/entidades/licenciaPorEntidad.ts
+++ src/entidades/licenciaPorEntidad.ts
@@ -1,3 +1,8 @@
+import { IDistribuidor } from './distribuidor';
+import { ILicencia } from './licencia';
+import { IProductor } from './productor';
+import { IQuimica } from './quimica';
+
 export interface ILicenciaPorEntidad {
   _id?: string;
   idLicencia?: string; // ID de la licencia
@@ -5,26 +10,26 @@ export interface ILicenciaPorEntidad {
   fechaCreacion?: string; // Fecha de creación de la licencia
   fechaExpiracion?: string; // Fecha de expiración de la licencia
   // Virtuals
-  quimica?: string; // ID de la química asociada
-  distribuidor?: string; // ID del distribuidor asociado
-  productor?: string; // ID del productor asociado
-  licencia?: string; // Nombre de la licencia
+  quimica?: IQuimica;
+  distribuidor?: IDistribuidor;
+  productor?: IProductor;
+  licencia?: ILicencia;
 }
 
 type OmitirCreate =
-  | "_id"
-  | "quimica"
-  | "distribuidor"
-  | "productor"
-  | "licencia";
+  | '_id'
+  | 'quimica'
+  | 'distribuidor'
+  | 'productor'
+  | 'licencia';
 export interface ICreateLicenciaPorEntidad
   extends Omit<Partial<ILicenciaPorEntidad>, OmitirCreate> {}
 
 type OmitirUpdate =
-  | "_id"
-  | "quimica"
-  | "distribuidor"
-  | "productor"
-  | "licencia";
+  | '_id'
+  | 'quimica'
+  | 'distribuidor'
+  | 'productor'
+  | 'licencia';
 export interface IUpdateLicenciaPorEntidad
   extends Omit<Partial<ILicenciaPorEntidad>, OmitirUpdate> {}
